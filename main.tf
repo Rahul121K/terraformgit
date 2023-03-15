83f664ebb4565a6acdc87b380ccfa2626bf45b31
Rahul121K <90963365+Rahul121K@users.noreply.github.com>
2023-03-15 09:16:18 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "bucket23" {
+	bucket = "rahul-first-st-bucket"
+}
+
+resource "aws_s3_bucket_acl" "rahul_bucket_acl" {
+bucket = aws_s3_bucket.bucket23.id
+acl = "private"
+}
+
+provider "aws" {
+    access_key=var.accesskey
+    secret_key=var.secretKey
+  region = "eu-west-1"
+}
+
+variable "accesskey"{
+    default="AKIAZTIMJ7JHDAJQWHUB"
+
+}
+
+variable "secretkey"{
+
+    default="DR1KGT+eMQ+7oVcAMcSIM5/ZsYJ8vLbuf23mG6Dt"
+}
